7d501b9b1dc24b2cfcb10d42d52dbe4f2df4d88b
lyzsk <sichu.huang2021@outlook.com>
2022-12-23 15:13:49 +0000

    add shopping cart controller

--- src/main/java/cn/sichu/fda/controller/ShoppingCartController.java
+++ src/main/java/cn/sichu/fda/controller/ShoppingCartController.java
@@ -1,7 +1,17 @@
 package cn.sichu.fda.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.sichu.fda.common.BaseContext;
+import cn.sichu.fda.common.Result;
+import cn.sichu.fda.entity.ShoppingCart;
+import cn.sichu.fda.service.IShoppingCartService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +21,98 @@ import org.springframework.web.bind.annotation.RestController;
  * @author sichu
  * @since 2022-12-20
  */
+@Slf4j
 @RestController
-@RequestMapping("/fda/shoppingCart")
+@RequestMapping("/shoppingCart")
 public class ShoppingCartController {
+    @Autowired
+    private IShoppingCartService shoppingCartService;
 
+    @PostMapping("/add")
+    public Result<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        log.info("shoppingCart: {}", shoppingCart);
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+        Long dishId = shoppingCart.getDishId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper =
+            new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+        if (dishId != null) {
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            queryWrapper.eq(ShoppingCart::getSetmealId,
+                shoppingCart.getSetmealId());
+        }
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+        if (cartServiceOne != null) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+        return Result.success(cartServiceOne);
+    }
+
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list() {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper =
+            new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId())
+            .orderByAsc(ShoppingCart::getCreateTime);
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+        return Result.success(list);
+    }
+
+    @Transactional
+    @DeleteMapping("/clean")
+    public Result<String> clean() {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper =
+            new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        shoppingCartService.remove(queryWrapper);
+        return Result.success("清空购物车成功");
+    }
+
+    @Transactional
+    @PostMapping("/sub")
+    public Result<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        Long dishId = shoppingCart.getDishId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper =
+            new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        if (dishId != null) {
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+            ShoppingCart cart1 = shoppingCartService.getOne(queryWrapper);
+            cart1.setNumber(cart1.getNumber() - 1);
+            Integer number1 = cart1.getNumber();
+            if (number1 > 0) {
+                shoppingCartService.updateById(cart1);
+            } else if (number1 == 0) {
+                shoppingCartService.removeById(cart1.getId());
+            } else {
+                return Result.error("操作异常");
+            }
+            return Result.success(cart1);
+        }
+        Long setmealId = shoppingCart.getSetmealId();
+        if (setmealId != null) {
+            queryWrapper.eq(ShoppingCart::getSetmealId, setmealId);
+            ShoppingCart cart2 = shoppingCartService.getOne(queryWrapper);
+            cart2.setNumber(cart2.getNumber() - 1);
+            Integer number2 = cart2.getNumber();
+            if (number2 > 0) {
+                shoppingCartService.updateById(cart2);
+            } else if (number2 == 0) {
+                shoppingCartService.removeById(cart2.getId());
+            } else {
+                return Result.error("操作异常");
+            }
+            return Result.success(cart2);
+        }
+        return Result.error("操作异常");
+    }
 }
